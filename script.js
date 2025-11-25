fa52cad1d366ffe9d095fdb81b55e52f25630564
dhulam07 <chandudhulam794@gmail.com>
2025-11-25 17:49:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //complete this code
 class Rectangle {
 	constructor(width,height){
-		this.width=width;
-		this.height=height;
+		this._width=width;
+		this._height=height;
 	}
 	get width(){
-		return (this.width);
+		return (this._width);
 	}
 	get height(){
-		return (this.height);
+		return (this._height);
 	}
 	getArea(){
 		console.log(this.width * this.height);
@@ -19,11 +19,11 @@ class Square extends Animal {
 	constructor(side){
 		super(side,side);
 	}
-	getArea(){
-		console.log(this.width * this.height);
+	get Area(){
+		return (this.width * this.height);
 	}
-	grtPerimeter(){
-		console.log(this.side*4);
+	grt Perimeter(){
+		return (this.side*4);
 	}
 }
 
